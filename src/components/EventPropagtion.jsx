8f08e520fb2a2d8bba4c4881a14671ec7b8c0731
JatinSharma <thisisjatinn@gmail.com>
2024-12-01 12:42:58 +0530

    onClickCapture to enable Capture phase : Top to bottom : GrandParent->Parent->Child

--- src/components/EventPropagtion.jsx
+++ src/components/EventPropagtion.jsx
@@ -1,29 +1,26 @@
 export const EventPropagtion = () => {
   const handleGrandParentClick = (e) => {
-    e.stopPropagation(); 
     console.log("Grand parent clicked");
   };
   const handleParentClick = (e) => {
-    e.stopPropagation(); 
     console.log("Parent clicked");
   };
   const handleChildClick = (e) => {
-    e.stopPropagation(); 
     console.log("Child clicked");
   };
 
   return (
     <div
       className="flex gap-5 bg-blue-500 px-10 py-5 w-fit"
-      onClick={handleGrandParentClick}
+      onClickCapture={handleGrandParentClick}
     >
       <div
         className="flex gap-5 bg-red-300 px-10 py-5"
-        onClick={handleParentClick}
+        onClickCapture={handleParentClick}
       >
         <div
           className="flex gap-5 bg-green-200 px-10 py-5 cursor-pointer"
-          onClick={handleChildClick}
+          onClickCapture={handleChildClick}
         >
           Click me!
         </div>
