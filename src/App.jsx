7179ccd10301fa4ba81e96fdd63fa6c9e4c48efc
JatinSharma <thisisjatinn@gmail.com>
2024-12-01 10:49:53 +0530

    Added Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return <hi>Welcome to React Learning</hi>;
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-white">
+        Integrated the Tailwind with React successfully
+      </h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure this includes all your React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
