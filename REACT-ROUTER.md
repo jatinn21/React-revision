7714b3794cbf475096989f7a14f48271b8597341
JatinSharma <thisisjatinn@gmail.com>
2024-12-02 22:29:17 +0530

    Basic setup of react router

---
+++ REACT-ROUTER.md
@@ -0,0 +1,4 @@
+# REACT ROUTER v6
+- Install using the following command
+npm install react-router --force ( if you are using react v19 or higher )
+
--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,48 @@
 
 // import UseRef from "./hooks/UseRef";
 
-import ForwardRef from "./hooks/ForwardRef";
+// import ForwardRef from "./hooks/ForwardRef";
 
-function App() {
-  return (
-    <>
-      <ForwardRef />
-    </>
-  );
+// function App() {
+//   return (
+//     <>
+//       <ForwardRef />
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+// ------------REACT ROUTER---------------
+
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import TermsAndConditions from "./components/TermsAndConditions";
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+    {
+      path: "/contact",
+      element: <Contact />,
+    },
+    {
+      path: '/terms-and-conditions',
+      element: <TermsAndConditions />,
+    }
+  ]);
+  return <RouterProvider router={router} />
 }
 
 export default App;
+
